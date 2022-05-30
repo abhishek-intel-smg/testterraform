423ac8691c6e0200ccce471255bff00d1d3101c9
abhishek-intel-smg <95842975+abhishek-intel-smg@users.noreply.github.com>
2022-05-30 15:52:51 +0530

    Update main.tf

--- GCP/main.tf
+++ GCP/main.tf
@@ -1,33 +1,45 @@
-# [START compute_flask_quickstart_vm]
-# Create a single Compute Engine instance
-provider "google" {
-  project     = "newdevopsintel"
-  region      = "us-west1"
-  zone        = "us-west1-a"
-}
-resource "google_compute_instance" "default" {
-  name         = "flask-vm"
-  machine_type = "f1-micro"
-  zone         = "us-west1-a"
-  tags         = ["ssh"]
+# Summary: Creates the simplest VM instance
 
-  metadata = {
-    enable-oslogin = "TRUE"
+# Documentation: https://www.terraform.io/docs/language/settings/index.html
+terraform {
+  required_version = ">= 1.2.1"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.18.0"
+    }
   }
+}
+
+# Documentation: https://www.terraform.io/docs/language/providers/requirements.html
+provider "google" {
+  project = var.project_id
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+# Documentation: https://www.terraform.io/docs/language/values/variables.html
+variable "project_id" {
+  type = string
+}
+
+# Compute Instance
+# Documentation: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance
+resource "google_compute_instance" "changeme_simple_instance" {
+  name         = "changeme-simple-instance"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "debian-cloud/debian-10"
     }
   }
 
-  # Install Flask
-  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
-
   network_interface {
+    # Explanation: A default network is created for all GCP projects
     network = "default"
-
     access_config {
-      # Include this section to give the VM an external IP address
     }
   }
 }
